ffadaeba6c36ad5c6824a1f79f67f986dc367683
Davi Mateus Gomes de Almeida <66326378+davimateus1@users.noreply.github.com>
2021-09-07 13:28:58 -0300

    Projeto criado

---
+++ backend/src/main/java/com/projectdavi/dmvendas/DmvendasApplication.java
@@ -0,0 +1,13 @@
+package com.projectdavi.dmvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projectdavi/dmvendas/DmvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectdavi.dmvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
