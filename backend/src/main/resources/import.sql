3b9af47d310a28b86f758b7607a41cf2c047efdc
Davi Mateus Gomes de Almeida <66326378+davimateus1@users.noreply.github.com>
2021-09-10 11:00:40 -0300

    Primeira requisição

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,8 +1,8 @@
-INSERT INTO tb_sellers(name) VALUES ('Logan');
-INSERT INTO tb_sellers(name) VALUES ('Anakin');
-INSERT INTO tb_sellers(name) VALUES ('BarryAllen');
-INSERT INTO tb_sellers(name) VALUES ('Kal-El');
-INSERT INTO tb_sellers(name) VALUES ('Padme');
+INSERT INTO tb_sellers(name) VALUES ('Davi');
+INSERT INTO tb_sellers(name) VALUES ('Djalma');
+INSERT INTO tb_sellers(name) VALUES ('Ronny');
+INSERT INTO tb_sellers(name) VALUES ('Rafael');
+INSERT INTO tb_sellers(name) VALUES ('João');
 
 INSERT INTO tb_sales(seller_id,visited,deals,amount,date) VALUES (5,83,66,5501.0,'2021-04-01');
 INSERT INTO tb_sales(seller_id,visited,deals,amount,date) VALUES (5,113,78,8290.0,'2021-03-31');
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,11 +1,30 @@
+import axios from "axios";
 import Chart from "react-apexcharts";
+import { BASE_URL } from "utils/requests";
+import { SaleSum } from "types/sale";
+
+type ChartData = {
+  labels: string[];
+  series: number[];
+};
 
 const DonutChart = () => {
+  //FORMA ERRADA
+  let chartData: ChartData = { labels: [], series: [] };
 
-  const mockData = {
-    series: [477138, 499928, 444867, 220426, 473088],
-    labels: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
-  };
+  axios.get(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
+    const data = response.data as SaleSum [];
+    const myLabels = data.map(x => x.sellerName);
+    const mySeries = data.map(x => x.sum);
+
+    chartData = { labels: myLabels, series: mySeries};
+    console.log(chartData);
+  });
+
+  //const mockData = {
+  // series: [477138, 499928, 444867, 220426, 473088],
+  // labels: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
+  //};
 
   const options = {
     legend: {
@@ -15,8 +34,8 @@ const DonutChart = () => {
 
   return (
     <Chart
-      options={{ ...options, labels: mockData.labels }}
-      series={mockData.series}
+      options={{ ...options, labels: chartData.labels }}
+      series={chartData.series}
       type="donut"
       height="240"
     />
