6cec1b5d785825dc2da6f67281f54c101cee0676
Davi Mateus Gomes de Almeida <66326378+davimateus1@users.noreply.github.com>
2021-09-10 13:25:02 -0300

    Integração Bar Chart

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,32 +1,70 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSuccess } from 'types/sale';
+import { round } from 'utils/format';
+import { BASE_URL } from 'utils/requests';
+
+type SeriesData = {
+    name: string;
+    data: number[];
+}
+
+type ChartData = {
+    labels: {
+        categories: string[];
+    };
+    series: SeriesData[];
+}
 
 const BarChart = () => {
-    const options = {
-        plotOptions: {
-            bar: {
-                horizontal: true,
-            }
-        },
-    };
-    
-    const mockData = {
+
+    const [chartData, setChartData] = useState<ChartData>({
         labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+            categories: []
         },
         series: [
             {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
+                name: "",
+                data: []               
             }
         ]
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/success-by-seller`).then((response) => {
+          const data = response.data as SaleSuccess[];
+          const myLabels = data.map((x) => x.sellerName);
+          const mySeries = data.map((x) => round(100.0 * x.deals / x.visited, 1));
+      
+          setChartData({
+            labels: {
+                categories: myLabels
+            },
+            series: [
+                {
+                    name: "% de Sucesso",
+                    data: mySeries              
+                }
+            ]
+        });
+        });
+      },[]);
+
+    const options = {
+        plotOptions: {
+            bar: {
+                horizontal: false,
+            }
+        },
     };
-    
+      
     return (
         <Chart 
-            options={{...options, xaxis: mockData.labels}}
-            series={mockData.series}
+            options={{...options, xaxis: chartData.labels}}
+            series={chartData.series}
             type="bar"
-            height="240"
+            height="250"
         />
     );
   }
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -20,15 +20,10 @@ const DonutChart = () => {
       const data = response.data as SaleSum[];
       const myLabels = data.map((x) => x.sellerName);
       const mySeries = data.map((x) => x.sum);
-  
+
       setChartData({ labels: myLabels, series: mySeries });
     });
-  },[]);
-
-  //const mockData = {
-  // series: [477138, 499928, 444867, 220426, 473088],
-  // labels: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
-  //};
+  }, []);
 
   const options = {
     legend: {
