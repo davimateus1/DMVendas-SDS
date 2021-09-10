79b20f718124b4ffa7ae1a839c0c79348e9fbba2
Davi Mateus Gomes de Almeida <66326378+davimateus1@users.noreply.github.com>
2021-09-10 10:01:47 -0300

    Navegação Home

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,4 +1,5 @@
-import ImgDmDark from 'assets/img/DMVendas.png';
+import ImgDmDark from "assets/img/DMVendas.png";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
@@ -6,12 +7,14 @@ const NavBar = () => {
       <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
         <div className="container">
           <nav className="my-2 my-md-0 mr-md-3">
-            <img src={ImgDmDark} alt="DevSuperior" width="120" />
+            <Link to="/">
+              <img src={ImgDmDark} alt="DMVendas" width="120" />
+            </Link>
           </nav>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default NavBar;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,30 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
-    return (
-      <div>
-          <h1>Página Home</h1>
+  return (
+    <>
+    <NavBar />
+      <div className="container text-center">
+        <div className="jumbotron">
+          <h1 className="display-4">DM Vendas</h1>
+          <p className="lead">
+            Analise o desempenho das suas vendas por diferentes perspectivas
+          </p>
+          <hr />
+          <p>
+            Esta aplicação consiste em exibir um dashboard a partir de dados
+            fornecidos por um back end construído com Spring Boot.
+          </p>
+          <Link to="/dashboard">
+          <button type="button" className="btn btn-lg btn-outline-danger col-md-12">Acessar Dashboard</button>
+          </Link>
+        </div>
       </div>
-    );
-  }
-  
-  export default Home;
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
