c706b3d32c81f25403f4e93a50393ece1dcde85d
Davi Mateus Gomes de Almeida <66326378+davimateus1@users.noreply.github.com>
2021-09-10 13:45:57 -0300

    Integração Data Table

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,26 @@
+import { Seller } from "./seller";
+
+export type Sale = {
+  id: number;
+  visited: number;
+  deals: number;
+  amount: number;
+  date: string;
+  seller: Seller;
+};
+
+export type SalePage = {
+  content?: Sale[];
+  last: boolean;
+  totalPages: number;
+  totalElements: number;
+  size?: number;
+  number: number;
+  numberOfElements?: number;
+  first: boolean;
+  empty?: boolean;
+};
+
 export type SaleSum = {
   sellerName: string;
   sum: number;
